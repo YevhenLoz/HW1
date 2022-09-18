e6c95567083a56e5f238e3b256b29a9be6a1b51d
Yevhen L <yevhen.lozovatslyi@gmail.com>
2022-09-18 09:59:27 +0300

    Added task 2

---
+++ 1/HW1.py
@@ -0,0 +1,58 @@
+#1. Задача: Створіть дві змінні first=10, second=30.
+# Виведіть на екран результат математичної взаємодії (+, -, *, / и тд.) для цих чисел.
+
+
+first = 10
+second = 30
+
+res = first + second
+print(res)
+
+res = first - second
+print(res)
+
+res = first * second
+print(res)
+
+res = first / second
+print(res)
+
+res = first ** second
+print(res)
+
+res = first // second
+print(res)
+
+res = first % second
+print(first)
+
+first += second
+print(first)
+
+first *= second
+print(first)
+
+first /= second
+print(first)
+
+
+# 2. Задача: Створіть змінну и по черзі запишіть в неї результат порівняння (<, > , ==, !=) чисел з завдання 1.
+# Виведіть на екран результат кожного порівняння.
+
+compare = first < second
+print(compare)
+
+compare = first > second
+print(compare)
+
+compare = first <= second
+print(compare)
+
+compare = first >= second
+print(compare)
+
+compare = first == second
+print(compare)
+
+compare = first != second
+print(compare)
